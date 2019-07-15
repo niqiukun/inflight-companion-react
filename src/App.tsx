b3d1a709c5000368117ebe79b828e27880f8929d
Ni Tianzhen <nitianzhen1210@gmail.com>
2019-07-15 12:24:19 +0800

    Remove progress bar due to flashing problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import { IonPage, IonRouterOutlet, IonProgressBar } from "@ionic/react";
-import './App.css';
+import { IonPage, IonRouterOutlet } from "@ionic/react";
+import "./App.css";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/core/css/core.css";
@@ -19,30 +19,25 @@ import "@ionic/core/css/text-transformation.css";
 import "@ionic/core/css/flex-utils.css";
 import "@ionic/core/css/display.css";
 
-const Home = React.lazy(() => import("./pages/Home"));
-const Menu = React.lazy(() => import("./pages/Menu"));
+import Menu from "./pages/Menu";
+import Home from "./pages/Home";
+
+// const Home = React.lazy(() => import("./pages/Home"));
+// const Menu = React.lazy(() => import("./pages/Menu"));
 
 const App: React.SFC = () => (
-    <Router>
-      {/* Conditional routing - If not authenticated, bring to login screen */}
-      <Route
-          exact
-          path="/"
-          render={() =>
-              <Redirect to="/home" />
-          }
-      />
-      <div className="App">
-        <IonPage>
-          <Suspense fallback={<IonProgressBar type="indeterminate"></IonProgressBar>}>
-            <IonRouterOutlet>
-              <Route path="/home" component={Home} exact={true} />
-              <Route path="/menu" component={Menu} exact={true} />
-            </IonRouterOutlet>
-          </Suspense>
-        </IonPage>
-      </div>
-    </Router>
+  <Router>
+    {/* Conditional routing - If not authenticated, bring to login screen */}
+    <Route exact path="/" render={() => <Redirect to="/home" />} />
+    <div className="App">
+      <IonPage>
+        <IonRouterOutlet>
+          <Route path="/home" component={Home} exact={true} />
+          <Route path="/menu" component={Menu} exact={true} />
+        </IonRouterOutlet>
+      </IonPage>
+    </div>
+  </Router>
 );
 
 export default App;
