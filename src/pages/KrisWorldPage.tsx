fca4b14d5e0555eba083b53132d072c0dd082dd8
Ni Tianzhen <nitianzhen1210@gmail.com>
2019-10-11 10:54:51 +0800

    Add placeholders for KrisWorld page

--- src/pages/KrisWorldPage.tsx
+++ src/pages/KrisWorldPage.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import {
   IonBackButton,
   IonButtons,
+  IonCol,
   IonContent,
   IonHeader,
+  IonListHeader,
+  IonRow,
   IonTitle,
   IonToolbar
 } from "@ionic/react";
@@ -21,8 +24,112 @@ const KrisWorldPage: React.FunctionComponent<RouteComponentProps<{}>> = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent class="dark">
+        <IonListHeader class="dark">Recommended</IonListHeader>
+        <IonRow style={{ padding: "0 6px" }}>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background:
+                  'darkgray url("/assets/img/aladdin_poster.jpg") 0 0/100%',
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}></IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}></IonCol>
+        </IonRow>
+        <IonListHeader class="dark">New Releases</IonListHeader>
+        <IonRow style={{ padding: "0 6px" }}>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background:
+                  'darkgray url("/assets/img/aladdin_poster.jpg") 0 0/100%',
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+        </IonRow>
+        <IonRow style={{ padding: "0 6px" }}>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+        </IonRow>
+        <IonRow style={{ padding: "0 6px" }}>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+          <IonCol size="4" style={{ padding: "8px" }}>
+            <div
+              style={{
+                background: "darkgray",
+                width: "100%",
+                paddingBottom: "150%"
+              }}
+            />
+          </IonCol>
+        </IonRow>
         <div style={{ color: "white", margin: "12px" }}>
-          Sorry, this feature is not available for demo :(
+          {/*Sorry, this feature is not available for demo :(*/}
         </div>
       </IonContent>
     </>
